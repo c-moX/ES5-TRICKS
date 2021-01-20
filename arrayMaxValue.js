4f9677b483a836e6ffa98c2e864180abf688e0d9
c-mox <cmoxsh@gmail.com>
2021-01-20 17:58:22 +0600

    added new code and file

---
+++ arrayMaxValue.js
@@ -0,0 +1,10 @@
+var marks = [10, 50, 20, 60, 30, 70, 40];
+var max = marks[0];
+
+for(var i = 0; i < marks.length; i++){
+    var element = marks[i];
+    if(element > max){
+        max = element;
+    }
+}
+console.log("Highest value is: ", max);
