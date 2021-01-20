7a4b3ea2291133088e5f767019f037942033c1b6
c-mox <cmoxsh@gmail.com>
2021-01-20 21:23:02 +0600

    Added new file

--- wordCount.js
+++ wordCount.js
@@ -4,7 +4,7 @@ var speech = "I am a good person. I don't snore at night.";
 var count = 0;
 for(var i = 0; i < speech.length; i++){
     var char = speech[i];
-    if(char == " " && speech[i-1] != " "){ // For the double space
+    if(char == " " && speech[i-1] != " "){ // For the double space/gap
         count++;
     }
 }
