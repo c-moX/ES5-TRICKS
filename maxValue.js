785559ee8fa29c4ecc523c4eae88c6291e204b79
c-mox <cmoxsh@gmail.com>
2021-01-20 17:21:23 +0600

    ES5 Tricks

---
+++ maxValue.js
@@ -0,0 +1,18 @@
+var business = 650;
+var minister = 750;
+var sochib = 550;
+
+if(business > minister){
+    if(business > sochib){
+        console.log("Business is bigger");
+    }else{
+        console.log("Sochib is bigger");
+    }
+}
+else{
+    if(minister > sochib){
+        console.log("Minister is bigger");
+    }else{
+        console.log("Sochib is bigger");
+    }
+}
---
+++ swap.js
@@ -0,0 +1,24 @@
+// Rule: 01(Any Language):
+var a = 5;
+var b = 7;
+console.log("Before swap: a =", a, "b =", b);
+var temp = a;
+a = b;
+b = temp;
+console.log("After swap: a =", a, "b =", b);
+
+// Rule: 02(Any Language):
+var x = 5;
+var y = 7;
+
+x = x + y;
+y = x - y;
+x = x - y;
+// console.log("After swap: x =", x, "y =", y);
+
+// Rule: 03(Only JavaScript):
+var p = 5;
+var q = 7;
+
+[p, q] [q, p];
+// console.log("After swap: p =", p, "q =", q);
